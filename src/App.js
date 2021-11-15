19c99a514ad36e449113de41df7a7d097cdfcc89
asrafilll <asrafilr@gmail.com>
2021-11-16 05:03:16 +0700

    architecure landing page first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" className="p-6 items-center justify-center">
+      <h1 className="text-red-700 font-extrabold">Hello World!</h1>
+      <p className="tracking-widest">This is my first React App.</p>
     </div>
   );
 }
